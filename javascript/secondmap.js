2ad0bc28fedf4c1998f0e93355cffc3c47ce0ec2
amaliebethune <71878499+amaliebethune@users.noreply.github.com>
2020-12-05 00:28:17 -0500

    kjkjbn

--- javascript/secondmap.js
+++ javascript/secondmap.js
@@ -1,4 +1,4 @@
-var firstmap = L.map('canadaMap').setView([ 62.558995, -97.005705], 3.00);
+var secondmap = L.map('canadaMap').setView([ 62.558995, -97.005705], 3.00);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/amaliebethune/ckib3rzev045i19t0h23q0kzc.html?fresh=true&title=view&access_token=pk.eyJ1IjoiYW1hbGllYmV0aHVuZSIsImEiOiJja2liM25vbHQwNTMzMnFwM3JibnlocnJtIn0.N01w-pVGqhkPof5ROJrb8A}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
